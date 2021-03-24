0b112a4a67f4e9834a0f501bc11ae0dbcf6d75bd
gil6464 <gil6464@gmail.com>
2021-03-24 23:00:53 +0200

    beckend test done! :)

--- app.js
+++ app.js
@@ -6,17 +6,16 @@ const url = process.env.MONGO_URI;
 const Tickets = require("./routes/tickets");
 
 app.use(express.static("client/build"));
-
+app.use(express.json());
 app.get("/api/tickets", async (req, res) => {
   try {
     let data = await Tickets.find({});
     const { searchText } = req.query;
     if (searchText) {
-      data =
-        data &&
-        data.filter(serach => {
-          return serach.title.toLowerCase().includes(searchText.toLowerCase());
-        });
+      const lowerCaseText = searchText.toLowerCase();
+      data = data.filter(serach => {
+        return serach.title.toLowerCase().includes(lowerCaseText);
+      });
     }
     res.json(data);
   } catch (e) {
@@ -26,12 +25,16 @@ app.get("/api/tickets", async (req, res) => {
       .json("There was a problem with our server, please try again later");
   }
 });
+app.patch("/api/tickets/:ticketId/done", (req, res) => {
+  const ticketId = req.params.ticketId;
+  Tickets.findByIdAndUpdate(ticketId, { done: true }, () => {
+    res.json({ updated: true });
+  });
+});
+app.patch("/api/tickets/:ticketId/undone", (req, res) => {
+  const ticketId = req.params.ticketId;
+  Tickets.findByIdAndUpdate(ticketId, { done: false }, () => {
+    res.json({ updated: true });
+  });
+});
 module.exports = app;
-//   data =
-//     data &&
-//     data.filter(serach => {
-//       return serach.title
-//         .toLowerCase()
-//         .includes(searchText.toLocaleLowerCase);
-//     });
-// }
